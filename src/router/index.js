aa8ee65d3bf9bc21d5a806d614ccd29b2c09c4b5
Артем Клечановский <m1sstake@MacBook-Pro-Artem.local>
2025-01-02 13:47:00 +0300

    fix: fix routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,9 @@ export const router = createRouter({
   history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      console.log(123);
-      return savedPosition;
-    }
     if (to.hash) {
       return { el: to.hash };
     }
-    return { x: 0, y: 0 };
+    window.scrollTo(0, 0);
   },
 });
